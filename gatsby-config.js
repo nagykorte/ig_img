204f8f8715f0bdfbb93e32ba878755f64c5fee6b
nagykorte <nicolasatilamatray@gmail.com>
2024-01-16 00:22:08 -0300

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `ig_img`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-sass"]
+};
